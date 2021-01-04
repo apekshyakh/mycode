dab4e587fd882eec6d933ad08d6adfa6d57e9e1e
Apekshya Khanal <apekshyakh@gmail.com>
2021-01-04 20:27:15 +0000

    challenge input()

--- lab_input/challenge.py
+++ lab_input/challenge.py
@@ -3,4 +3,14 @@ username = input("Please enter username:")
 day_of_week = input("What day of week is it?")
 print("Hello, " + username + "! Happy " + day_of_week + "!")
 
+# PRINT OBJECTS
+print("Hello ", user,"! Happy ",day,"!",sep="")
+# CONCATENATION
+print("Hello, " + user + "! Happy " + day + "!")
+# MIX AND MATCH!
+print("Hello", user +"!","Happy", day +"!")
+# FORMAT
+print("Hello {}! Happy {}!".format(user,day))
+# F-STRING
+print(f"Hello {user}! Happy {day}!")
 
