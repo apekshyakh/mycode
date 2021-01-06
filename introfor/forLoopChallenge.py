370e42d368558d7e1834e2947be63d01ee0254e6
Apekshya Khanal <apekshyakh@gmail.com>
2021-01-06 18:17:25 +0000

    files exercises

---
+++ introfor/forLoopChallenge.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+count =1;
+for x in range(5):
+    
+    print("* "*count+"\n")
+    count=count+1
+count = 4
+
+for y in range(4):
+    print("* " *count +"\n")
+    count = count -1
+    
